a4950e8b7d9ec658014f0584e334f833f2d377c1
Henry Lee <henryleeworld@gmail.com>
2026-03-31 00:21:10 +0800

    feat:升級至 laravel 12

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,15 +4,11 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     /**
      * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
     public function show(User $user)
     {
